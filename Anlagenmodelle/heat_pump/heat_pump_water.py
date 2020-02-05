ed888b2009c854a028526f1086c5e03e6a80a500
maltefritz <malte.fritz@web.de>
2020-02-05 14:25:00 +0100

    Add heat pump to Anlagenmodelle

---
+++ Anlagenmodelle/heat_pump/heat_pump_water.py
@@ -0,0 +1,212 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan  9 10:32:24 2020
+
+@author: Malte Fritz
+"""
+
+from tespy.networks import network
+from tespy.components import (sink, source, splitter, compressor, condenser,
+                              pump, heat_exchanger_simple, valve, drum, 
+                              heat_exchanger, cycle_closer)
+from tespy.connections import connection, ref
+from tespy.tools.characteristics import char_line
+from tespy.tools.characteristics import load_default_char as ldc
+
+import numpy as np
+import pandas as pd
+
+# %% network
+
+nw = network(fluids=['water', 'NH3', 'air'],
+                 T_unit='C', p_unit='bar', h_unit='kJ / kg', m_unit='kg / s')
+
+# %% components
+
+# sources & sinks
+cc = cycle_closer('coolant cycle closer')
+cb = source('consumer back flow')
+cf = sink('consumer feed flow')
+amb = source('ambient air')
+amb_out1 = sink('sink ambient 1')
+amb_out2 = sink('sink ambient 2')
+
+# ambient air system
+sp = splitter('splitter')
+pu = pump('pump')
+
+# consumer system
+
+cd = condenser('condenser')
+dhp = pump('district heating pump')
+cons = heat_exchanger_simple('consumer')
+
+# evaporator system
+
+ves = valve('valve')
+dr = drum('drum')
+ev = heat_exchanger('evaporator')
+su = heat_exchanger('superheater')
+erp = pump('evaporator reciculation pump')
+
+# compressor-system
+
+cp1 = compressor('compressor 1')
+cp2 = compressor('compressor 2')
+ic = heat_exchanger('intercooler')
+
+# %% connections
+
+# consumer system
+
+c_in_cd = connection(cc, 'out1', cd, 'in1')
+
+cb_dhp = connection(cb, 'out1', dhp, 'in1')
+dhp_cd = connection(dhp, 'out1', cd, 'in2')
+cd_cons = connection(cd, 'out2', cons, 'in1')
+cons_cf = connection(cons, 'out1', cf, 'in1')
+
+nw.add_conns(c_in_cd, cb_dhp, dhp_cd, cd_cons, cons_cf)
+
+# connection condenser - evaporator system
+
+cd_ves = connection(cd, 'out1', ves, 'in1')
+
+nw.add_conns(cd_ves)
+
+# evaporator system
+
+ves_dr = connection(ves, 'out1', dr, 'in1')
+dr_erp = connection(dr, 'out1', erp, 'in1')
+erp_ev = connection(erp, 'out1', ev, 'in2')
+ev_dr = connection(ev, 'out2', dr, 'in2')
+dr_su = connection(dr, 'out2', su, 'in2')
+
+nw.add_conns(ves_dr, dr_erp, erp_ev, ev_dr, dr_su)
+
+amb_p = connection(amb, 'out1', pu, 'in1')
+p_sp = connection(pu, 'out1', sp, 'in1')
+sp_su = connection(sp, 'out1', su, 'in1')
+su_ev = connection(su, 'out1', ev, 'in1')
+ev_amb_out = connection(ev, 'out1', amb_out1, 'in1')
+
+nw.add_conns(amb_p, p_sp, sp_su, su_ev, ev_amb_out)
+
+# connection evaporator system - compressor system
+
+su_cp1 = connection(su, 'out2', cp1, 'in1')
+
+nw.add_conns(su_cp1)
+
+# compressor-system
+
+cp1_he = connection(cp1, 'out1', ic, 'in1')
+he_cp2 = connection(ic, 'out1', cp2, 'in1')
+cp2_c_out = connection(cp2, 'out1', cc, 'in1')
+
+sp_ic = connection(sp, 'out2', ic, 'in2')
+ic_out = connection(ic, 'out2', amb_out2, 'in1')
+
+nw.add_conns(cp1_he, he_cp2, sp_ic, ic_out, cp2_c_out)
+
+# %% component parametrization
+
+# condenser system
+
+cd.set_attr(pr1=0.99, pr2=0.99, ttd_u=5, design=['pr2', 'ttd_u'], offdesign=['zeta2', 'kA'])
+dhp.set_attr(eta_s=0.8, design=['eta_s'], offdesign=['eta_s_char'])
+cons.set_attr(pr=0.99, design=['pr'], offdesign=['zeta'])
+
+# ambient air
+
+pu.set_attr(eta_s=0.5, pr=1.005, design=['eta_s'], offdesign=['eta_s_char'])
+
+# evaporator system
+
+kA_char1 = ldc('heat exchanger', 'kA_char1', 'EVAPORATING FLUID', char_line)
+kA_char2 = ldc('heat exchanger', 'kA_char2', 'EVAPORATING FLUID', char_line)
+
+ev.set_attr(pr1=0.999, pr2=0.99, ttd_l=5,
+            kA_char1=kA_char1, kA_char2=kA_char2,
+            design=['pr1', 'ttd_l'], offdesign=['zeta1', 'kA'])
+su.set_attr(pr1=0.999, pr2=0.99, ttd_u=2, design=['pr1', 'pr2', 'ttd_u'], 
+            offdesign=['zeta1', 'zeta2', 'kA'])
+erp.set_attr(eta_s=0.8, design=['eta_s'], offdesign=['eta_s_char'])
+
+# compressor system
+
+x = np.array([0.000, 0.400, 1.000, 1.500])
+y = np.array([0.500, 0.900, 1.000, 1.025])
+gen = char_line(x=x, y=y)
+cp1.set_attr(eta_s=0.8, design=['eta_s'], offdesign=['eta_s_char'],
+             eta_s_char=gen)
+cp2.set_attr(eta_s=0.8, pr=5, design=['eta_s'], offdesign=['eta_s_char'],
+             eta_s_char=gen)
+ic.set_attr(pr1=0.98, pr2=0.999, design=['pr1', 'pr2'],
+            offdesign=['zeta1', 'zeta2', 'kA'])
+
+# %% connection parametrization
+
+# condenser system
+
+c_in_cd.set_attr(fluid={'air': 0, 'NH3': 1, 'water': 0})
+cb_dhp.set_attr(T=60, p=10, fluid={'air': 0, 'NH3': 0, 'water': 1})
+cd_cons.set_attr(T=90)
+cons_cf.set_attr(h=ref(cb_dhp, 1, 0), p=ref(cb_dhp, 1, 0))
+
+# evaporator system cold side
+
+erp_ev.set_attr(m=ref(ves_dr, 4, 0), p0=5)
+su_cp1.set_attr(p0=5, h0=1700)
+
+# evaporator system hot side
+
+amb_p.set_attr(T=12, p=4, fluid={'air': 0, 'NH3': 0, 'water': 1},
+                 offdesign=['v'])
+sp_su.set_attr(offdesign=['v'])
+ev_amb_out.set_attr(T=9, design=['T'])
+
+# compressor-system
+
+he_cp2.set_attr(T=40, p0=10, design=['T'])
+ic_out.set_attr(T=30, design=['T'])
+
+# %% key paramter
+
+cons.set_attr(Q=-200e3)
+
+# %% Calculation
+
+nw.solve('design')
+nw.print_results()
+nw.save('heat_pump_water')
+
+T_range = [6, 9, 12, 15, 18, 21, 24]
+Q_range = np.array([120e3, 140e3, 160e3, 180e3, 200e3, 220e3])
+df = pd.DataFrame(columns=Q_range / -cons.Q.val)
+
+for T in T_range:
+    amb_p.set_attr(T=T)
+    eps = []
+
+    for Q in Q_range:
+        cons.set_attr(Q=-Q)
+        try:
+            nw.solve('offdesign',
+                     init_path='OD_water_' + str(Q/1e3),
+                     design_path='heat_pump_water/results.csv')
+        except:
+            nw.solve('offdesign', init_path='heat_pump_water',
+                     design_path='heat_pump_water')
+
+        if nw.lin_dep:
+            eps += [np.nan]
+
+        else:
+            nw.save('OD_water_' + str(Q/1e3))
+            eps += [abs(cd.Q.val) / (cp1.P.val + cp2.P.val + erp.P.val +
+                    pu.P.val)]
+
+    df.loc[T] = eps
+
+df.to_csv('COP_water.csv')
