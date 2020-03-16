43ebb6e24775d08cf12998b4cd99b5f4a4fb715c
jfreissmann <j.freissmann@web.de>
2020-03-16 14:56:13 +0100

    Improve code style of invest.py

--- postprocessing/invest.py
+++ postprocessing/invest.py
@@ -1,8 +1,8 @@
-# -*- coding: utf-8 -*-
-"""
+"""Functions for economical analysis and assesment.
+
 Created on Thu Jan 16 10:13:55 2020
 
-@author: Malte Fritz
+@author: Markus Brandt, Malte Fritz & Jonas Freißmann
 """
 
 import numpy as np
@@ -10,27 +10,26 @@ import numpy as np
 
 # %% Kapitalwert
 def npv(invest, objective, i=0.05, n=20):
-    """
-    Konstantin 2013, Markus [29]
+    """Konstantin 2013, Markus [29].
+
     npv:        Kapitalwert (netpresentvalue)
     invest:     Investitionsausgaben zum Zeitpunkt t=0
     objective:  Zielwertfunktion - Zahlungsströme
     i:          Kalkulationszinssatz
     n:          Betrachtungsdauer
     bwsf:       Barwert Summenfaktor
-    """   
-    
+    """
     q = 1+i
     bwsf = (q**n - 1)/(q**n * (q - 1))
-    
+
     npv = -invest + bwsf * objective
     return npv
 
 
 # %% Wärmegestehungskosten
 def LCOH(invest, ausgaben, Q, i=0.05, n=20):
-    """
-    Konstantin 2013, Markus [29]
+    """Konstantin 2013, Markus [29].
+
     LCOH        Wärmegestehungskosten
     invest:     Investitionsausgaben zum Zeitpunkt t=0
     ausgaben:   Gesamtkosten
@@ -38,24 +37,22 @@ def LCOH(invest, ausgaben, Q, i=0.05, n=20):
     i:          Kalkulationszinssatz
     n:          Betrachtungsdauer
     """
-    
     q = 1 + i
     factor = (q**n * (q - 1)) / (q**n - 1)
-    
+
     LCOH = (invest * factor + ausgaben) / Q
     return LCOH
 
 
 # %% Investitionskosten
 def invest_st(A, col_type=''):
-    """
-    Pehnt et al. 2017, Markus [38]
+    """Pehnt et al. 2017, Markus [38].
+
     A:                Kollektorfläche der Solarthermie
     col_type:         Kollektortyp der Solarthermie
     specific_coasts:  Spezifische Kosten
     invest:           Investitionskosten
     """
-    
     if col_type == 'flat':
         specific_costs = -34.06 * np.log(A) + 592.48
         invest = A * specific_costs
@@ -69,13 +66,13 @@ def invest_st(A, col_type=''):
 
 
 def invest_stes(Q, sponsorship=0.3):
-    """
+    """Investment calculation for seasonal thermal energy storages.
+
     Q:              Kapazität des Speichers in MWh
-    sponsorship:    rel. Förderung des Speichers (durch Bundesamt für 
+    sponsorship:    rel. Förderung des Speichers (durch Bundesamt für
                     Wirtschaft und Ausfuhrkontrolle [10])
     q_V:            spez. volumetrische Energie
     """
-    
     # V = Q / 0.3
     q_V = 0.07    # MWh/m³, unsere Annahme (siehe Whiteboardbild)
     V_stes = Q / q_V
