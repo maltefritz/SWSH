20913ce1d1072e128825a137b179c8c2492984e3
maltefritz <malte.fritz@web.de>
2021-02-12 10:03:44 +0100

    Upgrate hp to tespy 0.4.0 and create document model for hp

--- Anlagenmodelle/hp/heat_pump_water040.py
+++ Anlagenmodelle/hp/heat_pump_water040.py
@@ -6,13 +6,14 @@ Created on Thu Jan  9 10:07:02 2020
 """
 import os.path as path
 
-from tespy.components import (sink, source, splitter, compressor, condenser,
-                              pump, heat_exchanger_simple, valve, drum,
-                              heat_exchanger, cycle_closer)
-from tespy.connections import connection, ref, bus
-from tespy.networks import network
-from tespy.tools.characteristics import char_line
+from tespy.components import (Sink, Source, Splitter, Compressor, Condenser,
+                              Pump, HeatExchangerSimple, Valve, Drum,
+                              HeatExchanger, CycleCloser)
+from tespy.connections import Bus, Ref, Connection
+from tespy.networks import Network
+from tespy.tools.characteristics import CharLine
 from tespy.tools.characteristics import load_default_char as ldc
+from tespy.tools import document_model
 
 import numpy as np
 import pandas as pd
@@ -31,94 +32,94 @@ T_amb_out = T_amb - 4
 
 # %% network
 
-nw = network(fluids=['water', 'NH3', 'air'], T_unit='C', p_unit='bar',
+nw = Network(fluids=['water', 'NH3', 'air'], T_unit='C', p_unit='bar',
              h_unit='kJ / kg', m_unit='kg / s')
 
 # %% components
 
 # sources & sinks
-cc = cycle_closer('coolant cycle closer')
-cb = source('consumer back flow')
-cf = sink('consumer feed flow')
-amb = source('ambient air')
-amb_out1 = sink('sink ambient 1')
-amb_out2 = sink('sink ambient 2')
+cc = CycleCloser('coolant cycle closer')
+cb = Source('consumer back flow')
+cf = Sink('consumer feed flow')
+amb = Source('ambient air')
+amb_out1 = Sink('sink ambient 1')
+amb_out2 = Sink('sink ambient 2')
 
 # ambient air system
-sp = splitter('splitter')
-pu = pump('pump')
+sp = Splitter('splitter')
+pu = Pump('pump')
 
 # consumer system
 
-cd = condenser('condenser')
-dhp = pump('district heating pump')
-cons = heat_exchanger_simple('consumer')
+cd = Condenser('condenser')
+dhp = Pump('district heating pump')
+cons = HeatExchangerSimple('consumer')
 
 # evaporator system
 
-ves = valve('valve')
-dr = drum('drum')
-ev = heat_exchanger('evaporator')
-su = heat_exchanger('superheater')
-erp = pump('evaporator reciculation pump')
+ves = Valve('valve')
+dr = Drum('drum')
+ev = HeatExchanger('evaporator')
+su = HeatExchanger('superheater')
+erp = Pump('evaporator reciculation pump')
 
 # compressor-system
 
-cp1 = compressor('compressor 1')
-cp2 = compressor('compressor 2')
-ic = heat_exchanger('intercooler')
+cp1 = Compressor('compressor 1')
+cp2 = Compressor('compressor 2')
+ic = HeatExchanger('intercooler')
 
 # %% connections
 
 # consumer system
 
-c_in_cd = connection(cc, 'out1', cd, 'in1')
+c_in_cd = Connection(cc, 'out1', cd, 'in1')
 
-cb_dhp = connection(cb, 'out1', dhp, 'in1')
-dhp_cd = connection(dhp, 'out1', cd, 'in2')
-cd_cons = connection(cd, 'out2', cons, 'in1')
-cons_cf = connection(cons, 'out1', cf, 'in1')
+cb_dhp = Connection(cb, 'out1', dhp, 'in1')
+dhp_cd = Connection(dhp, 'out1', cd, 'in2')
+cd_cons = Connection(cd, 'out2', cons, 'in1')
+cons_cf = Connection(cons, 'out1', cf, 'in1')
 
 nw.add_conns(c_in_cd, cb_dhp, dhp_cd, cd_cons, cons_cf)
 
 # connection condenser - evaporator system
 
-cd_ves = connection(cd, 'out1', ves, 'in1')
+cd_ves = Connection(cd, 'out1', ves, 'in1')
 
 nw.add_conns(cd_ves)
 
 # evaporator system
 
-ves_dr = connection(ves, 'out1', dr, 'in1')
-dr_erp = connection(dr, 'out1', erp, 'in1')
-erp_ev = connection(erp, 'out1', ev, 'in2')
-ev_dr = connection(ev, 'out2', dr, 'in2')
-dr_su = connection(dr, 'out2', su, 'in2')
+ves_dr = Connection(ves, 'out1', dr, 'in1')
+dr_erp = Connection(dr, 'out1', erp, 'in1')
+erp_ev = Connection(erp, 'out1', ev, 'in2')
+ev_dr = Connection(ev, 'out2', dr, 'in2')
+dr_su = Connection(dr, 'out2', su, 'in2')
 
 nw.add_conns(ves_dr, dr_erp, erp_ev, ev_dr, dr_su)
 
-amb_pu = connection(amb, 'out1', pu, 'in1')
-pu_sp = connection(pu, 'out1', sp, 'in1')
-sp_su = connection(sp, 'out1', su, 'in1')
-su_ev = connection(su, 'out1', ev, 'in1')
-ev_amb_out = connection(ev, 'out1', amb_out1, 'in1')
+amb_pu = Connection(amb, 'out1', pu, 'in1')
+pu_sp = Connection(pu, 'out1', sp, 'in1')
+sp_su = Connection(sp, 'out1', su, 'in1')
+su_ev = Connection(su, 'out1', ev, 'in1')
+ev_amb_out = Connection(ev, 'out1', amb_out1, 'in1')
 
 nw.add_conns(amb_pu, pu_sp, sp_su, su_ev, ev_amb_out)
 
 # connection evaporator system - compressor system
 
-su_cp1 = connection(su, 'out2', cp1, 'in1')
+su_cp1 = Connection(su, 'out2', cp1, 'in1')
 
 nw.add_conns(su_cp1)
 
 # compressor-system
 
-cp1_he = connection(cp1, 'out1', ic, 'in1')
-he_cp2 = connection(ic, 'out1', cp2, 'in1')
-cp2_c_out = connection(cp2, 'out1', cc, 'in1')
+cp1_he = Connection(cp1, 'out1', ic, 'in1')
+he_cp2 = Connection(ic, 'out1', cp2, 'in1')
+cp2_c_out = Connection(cp2, 'out1', cc, 'in1')
 
-sp_ic = connection(sp, 'out2', ic, 'in2')
-ic_out = connection(ic, 'out2', amb_out2, 'in1')
+sp_ic = Connection(sp, 'out2', ic, 'in2')
+ic_out = Connection(ic, 'out2', amb_out2, 'in1')
 
 nw.add_conns(cp1_he, he_cp2, sp_ic, ic_out, cp2_c_out)
 
@@ -134,18 +135,19 @@ y = 1 / (np.array([0.01, 0.3148, 0.5346, 0.6843, 0.7835, 0.8477, 0.8885,
                    0.9947, 0.9909, 0.9853, 0.9644])
          * 0.98)
 
-mot1 = char_line(x=x, y=y)
-mot2 = char_line(x=x, y=y)
-mot3 = char_line(x=x, y=y)
-mot4 = char_line(x=x, y=y)
-mot5 = char_line(x=x, y=y)
+mot1 = CharLine(x=x, y=y)
+mot2 = CharLine(x=x, y=y)
+mot3 = CharLine(x=x, y=y)
+mot4 = CharLine(x=x, y=y)
+mot5 = CharLine(x=x, y=y)
 
-power = bus('total compressor power')
-power.add_comps({'c': cp1, 'char': mot1}, {'c': cp2, 'char': mot2},
-                {'c': pu, 'char': mot3}, {'c': dhp, 'char': mot4},
-                {'c': erp, 'char': mot5})
-heat = bus('total delivered heat')
-heat.add_comps({'c': cd})
+power = Bus('total compressor power')
+power.add_comps({'comp': cp1, 'char': mot1}, {'comp': cp2, 'char': mot2},
+                {'comp': pu, 'char': mot3}, {'comp': dhp, 'char': mot4},
+                {'comp': erp, 'char': mot5})
+
+heat = Bus('total delivered heat')
+heat.add_comps({'comp': cd})
 
 nw.add_busses(power, heat)
 
@@ -164,8 +166,8 @@ pu.set_attr(eta_s=0.75, design=['eta_s'], offdesign=['eta_s_char'])
 
 # evaporator system
 
-kA_char1 = ldc('heat exchanger', 'kA_char1', 'DEFAULT', char_line)
-kA_char2 = ldc('heat exchanger', 'kA_char2', 'EVAPORATING FLUID', char_line)
+kA_char1 = ldc('heat exchanger', 'kA_char1', 'DEFAULT', CharLine)
+kA_char2 = ldc('heat exchanger', 'kA_char2', 'EVAPORATING FLUID', CharLine)
 
 ev.set_attr(pr1=0.98, pr2=0.99, ttd_l=5,
             kA_char1=kA_char1, kA_char2=kA_char2,
@@ -188,12 +190,12 @@ ic.set_attr(pr1=0.99, pr2=0.98, design=['pr1', 'pr2'],
 c_in_cd.set_attr(fluid={'air': 0, 'NH3': 1, 'water': 0})
 cb_dhp.set_attr(T=T_DH_rl, p=10, fluid={'air': 0, 'NH3': 0, 'water': 1})
 cd_cons.set_attr(T=T_DH_vl)
-cons_cf.set_attr(h=ref(cb_dhp, 1, 0), p=ref(cb_dhp, 1, 0))
+cons_cf.set_attr(h=Ref(cb_dhp, 1, 0), p=Ref(cb_dhp, 1, 0))
 
 # evaporator system cold side
 
-erp_ev.set_attr(m=ref(ves_dr, 1.25, 0), p0=5)
-su_cp1.set_attr(p0=5, h0=1700)
+erp_ev.set_attr(m=Ref(ves_dr, 1.25, 0), p0=5)
+su_cp1.set_attr(p0=5, state='g')
 
 # evaporator system hot side
 
@@ -218,10 +220,11 @@ nw.solve('design')
 nw.print_results()
 nw.save('hp_water')
 
-cp1.eta_s_char.func.extrapolate = True
-cp2.eta_s_char.func.extrapolate = True
+cp1.eta_s_char.char_func.extrapolate = True
+cp2.eta_s_char.char_func.extrapolate = True
 
 nw.solve('offdesign', design_path='hp_water')
+document_model(nw)
 nw.set_attr(iterinfo=False)
 
 T_db = []
@@ -272,10 +275,10 @@ for T in T_range:
                 P_list += [power.P.val]
 
                 Q_source = abs(ev.Q.val + su.Q.val)
-                SQ_source = abs(ev.SQ2.val + su.SQ2.val)
+                SQ_source = abs(ev.S_Q2 + su.S_Q2)
 
                 Q_sink = abs(cd.Q.val)
-                SQ_sink = abs(cd.SQ1.val)
+                SQ_sink = abs(cd.S_Q1)
 
                 Q_range += [heat.P.val]
 
@@ -312,10 +315,10 @@ for T in T_range:
                 P_list += [power.P.val]
 
                 Q_source = abs(ev.Q.val + su.Q.val)
-                SQ_source = abs(ev.SQ2.val + su.SQ2.val)
+                SQ_source = abs(ev.S_Q2 + su.S_Q2)
 
                 Q_sink = abs(cd.Q.val)
-                SQ_sink = abs(cd.SQ1.val)
+                SQ_sink = abs(cd.S_Q1)
 
                 Q_range += [heat.P.val]
 
