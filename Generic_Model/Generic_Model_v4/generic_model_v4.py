5d450627a5b14cc9f1a3529cf00574c949961ee5
jfreissmann <j.freissmann@web.de>
2021-11-02 15:04:18 +0100

    Move postprocessing of the optimization in to seperate file

--- Generic_Model/Generic_Model_v4/generic_model_v4.py
+++ Generic_Model/Generic_Model_v4/generic_model_v4.py
@@ -16,12 +16,9 @@ Wärmebedarf Flensburgs aus dem Jahr 2016
 
 """
 import pandas as pd
-import numpy as np
 
 import oemof.solph as solph
-from oemof.solph import views
-from help_funcs import topology_check, generate_labeldict, result_labelling
-from eco_funcs import invest_sol, invest_stes
+from help_funcs import topology_check
 from components import (
     gas_source, electricity_source, solar_thermal_strand, must_run_source
     )
@@ -38,19 +35,20 @@ from components import (
     seasonal_thermal_energy_storage_strand,
     short_term_thermal_energy_storage
     )
+from postprocessing import postprocessing
 
 
-def main(data, param, mipgap='0.1'):
+def main(param, data, mipgap='0.1'):
     """
     Execute main script.
 
     Parameters
     ----------
-    data : pandas.DataFrame
-        csv file of user defined time dependent parameters.
-
     param : dict
         JSON parameter file of user defined constants.
+
+    data : pandas.DataFrame
+        csv file of user defined time dependent parameters.
 
     mipgap : str
         termination criterion for gap between current and optimal solution.
@@ -139,306 +137,14 @@ def main(data, param, mipgap='0.1'):
     energysystem.results['main'] = solph.processing.results(model)
     energysystem.results['meta'] = solph.processing.meta_results(model)
 
-    cost_df = pd.DataFrame()
-    labeldict = {}
-
-    # Busses
-    data_gnw = views.node(results, 'Gasnetzwerk')['sequences']
-    data_enw = views.node(results, 'Elektrizitätsnetzwerk')['sequences']
-    data_wnw = views.node(results, 'Wärmenetzwerk')['sequences']
-    data_lt_wnw = views.node(results, 'LT-Wärmenetzwerk')['sequences']
-    data_wnw_node = views.node(results, 'TES Knoten')['sequences']
-    data_sol_node = views.node(results, 'Sol Knoten')['sequences']
-
-    if param['Sol']['active']:
-        data_solar_source = views.node(results, 'Solarthermie')['sequences']
-        cost_df.loc['invest', 'Sol'] = (
-            invest_sol(param['Sol']['A'], col_type='flat')
-            )
-        cost_df.loc['op_cost', 'Sol'] = (
-            data_solar_source[(('Solarthermie', 'Sol Knoten'), 'flow')].sum()
-            * 0.01 * cost_df.loc['invest', 'Sol']
-            / param['Sol']['A']*data['solar_data'].sum()
-            )
-
-    # Transformer
-    if param['EHK']['active']:
-        cost_df.loc['invest', 'EHK'] = (
-            param['EHK']['inv_spez']
-            * param['EHK']['Q_N']
-            * param['EHK']['amount']
-            )
-
-        for i in range(1, param['EHK']['amount']+1):
-            label_id = 'Elektroheizkessel_' + str(i)
-
-            cost_df.loc['op_cost', 'EHK'] = (
-                data_wnw[((label_id, 'Wärmenetzwerk'), 'flow')].sum()
-                * param['EHK']['op_cost_var']
-                + (param['EHK']['op_cost_fix']
-                   * param['EHK']['Q_N'])
-                )
-
-    if param['SLK']['active']:
-        cost_df.loc['invest', 'SLK'] = (
-            param['SLK']['inv_spez']
-            * param['SLK']['Q_N']
-            * param['SLK']['amount']
-            )
-
-        for i in range(1, param['SLK']['amount']+1):
-            label_id = 'Spitzenlastkessel_' + str(i)
-
-            cost_df.loc['op_cost', 'SLK'] = (
-                data_wnw[((label_id, 'Wärmenetzwerk'), 'flow')].sum()
-                * (param['SLK']['op_cost_var']
-                   + param['param']['energy_tax'])
-                + (param['SLK']['op_cost_fix']
-                   * param['SLK']['Q_N'])
-                )
-
-    if param['BHKW']['active']:
-        if param['BHKW']['type'] == 'constant':
-            ICE_P_max_woDH = param['BHKW']['P_max_woDH']
-        elif param['BHKW']['type'] == 'time series':
-            ICE_P_max_woDH = data['ICE_P_max_woDH'].mean()
-
-        cost_df.loc['invest', 'BHKW'] = (
-            ICE_P_max_woDH
-            * param['BHKW']['inv_spez']
-            * param['BHKW']['amount']
-            )
-
-        for i in range(1, param['BHKW']['amount']+1):
-            label_id = 'BHKW_' + str(i)
-
-            cost_df.loc['op_cost', 'BHKW'] = (
-                data_enw[((label_id, 'Elektrizitätsnetzwerk'), 'flow')].sum()
-                * param['BHKW']['op_cost_var']
-                + (param['BHKW']['op_cost_fix']
-                   * ICE_P_max_woDH)
-                )
-
-    if param['GuD']['active']:
-        if param['GuD']['type'] == 'constant':
-            CCET_P_max_woDH = param['GuD']['P_max_woDH']
-        elif param['GuD']['type'] == 'time series':
-            CCET_P_max_woDH = data['CCET_P_max_woDH'].mean()
-
-        cost_df.loc['invest', 'GuD'] = (
-            CCET_P_max_woDH
-            * param['GuD']['inv_spez']
-            * param['GuD']['amount']
-            )
-
-        for i in range(1, param['GuD']['amount']+1):
-            label_id = 'GuD_' + str(i)
-
-            cost_df.loc['op_cost', 'GuD'] = (
-                data_enw[((label_id, 'Elektrizitätsnetzwerk'), 'flow')].sum()
-                * param['GuD']['op_cost_var']
-                + (param['GuD']['op_cost_fix']
-                   * CCET_P_max_woDH)
-                )
-
-    if param['HP']['active']:
-        if param['HP']['type'] == 'constant':
-            HP_Q_N = (param['HP']['c_1']
-                      * param['HP']['P_max']
-                      + param['HP']['c_0'])
-        elif param['HP']['type'] == 'time series':
-            HP_Q_N = (data['c_1_hp'].mean()
-                      * data['P_max_hp'].mean()
-                      + data['c_0_hp'].mean())
-        cost_df.loc['invest', 'HP'] = (
-            param['HP']['inv_spez']
-            * data['P_max_hp'].max()
-            * HP_Q_N
-            * param['HP']['amount']
-            )
-        for i in range(1, param['HP']['amount']+1):
-            label_id = 'HP_' + str(i)
-
-            cost_df.loc['op_cost', 'HP'] = (
-                data_wnw[((label_id, 'Wärmenetzwerk'), 'flow')].sum()
-                * param['HP']['op_cost_var']
-                + param['HP']['op_cost_fix'] * HP_Q_N
-                )
-
-    if param['LT-HP']['active']:
-        LT_HP_Q_N = data_wnw[((label_id, 'Wärmenetzwerk'), 'flow')].mean()
-
-        for i in range(1, param['LT-HP']['amount']+1):
-            label_id = 'LT-HP_' + str(i)
-
-            cost_df.loc['invest', 'LT-HP'] = (
-                param['HP']['inv_spez'] * LT_HP_Q_N
-                )
-
-            cost_df.loc['op_cost', 'LT-HP'] = (
-                data_wnw[((label_id, 'Wärmenetzwerk'), 'flow')].sum()
-                * param['HP']['op_cost_var']
-                + param['HP']['op_cost_fix'] * LT_HP_Q_N
-                )
-
-    # Speicher
-    data_tes = pd.DataFrame()
-    if param['TES']['active']:
-        cost_df.loc['invest', 'TES'] = (
-            invest_stes(param['TES']['Q'])
-            * param['TES']['amount']
-            )
-
-        for i in range(1, param['TES']['amount']+1):
-            label_id = 'TES_' + str(i)
-            data_tes = pd.concat([data_tes,
-                                  views.node(results, label_id)['sequences']],
-                                 axis=1)
-
-            cost_df.loc['op_cost', 'TES'] = (
-                data_tes[(('TES Knoten', label_id), 'flow')].sum()
-                * param['TES']['op_cost_var']
-                + (param['TES']['op_cost_fix']
-                   * param['TES']['Q'])
-                )
-
-    if param['ST-TES']['active']:
-        cost_df.loc['invest', 'ST-TES'] = (
-            invest_stes(param['ST-TES']['Q'])
-            * param['ST-TES']['amount']
-            )
-
-        for i in range(1, param['ST-TES']['amount']+1):
-            label_id = 'ST-TES_' + str(i)
-            data_tes = pd.concat([data_tes,
-                                  views.node(results, label_id)['sequences']],
-                                 axis=1)
-
-            cost_df.loc['op_cost', 'ST-TES'] = (
-                data_tes[(('Wärmenetzwerk', label_id), 'flow')].sum()
-                * param['ST-TES']['op_cost_var']
-                + (param['ST-TES']['op_cost_fix']
-                   * param['ST-TES']['Q'])
-                )
-
-        # %% Geldflüsse
-
-    cost_Anlagen = cost_df.loc['op_cost'].sum()
-    invest_ges = cost_df.loc['invest'].sum()
-    # # Primärenergiebezugskoste
-    cost_gas = (data_gnw[(('Gasquelle', 'Gasnetzwerk'), 'flow')].sum()
-                * (param['param']['gas_price']
-                   + (param['param']['co2_price']
-                      * param['param']['ef_gas'])))
-
-    specific_costs_el_grid = (
-        param['param']['elec_consumer_charges_grid']
-        + data['el_spot_price']
-        + (param['param']['co2_price']
-           * data['ef_om'])
-        )
-    cost_el_grid = (
-        np.array(data_enw[(('Stromquelle', 'Elektrizitätsnetzwerk'), 'flow')])
-        * specific_costs_el_grid)
-    cost_el_grid = cost_el_grid.sum()
-
-    cost_el_internal = ((
-        data_enw.loc[
-            :, ['BHKW' in col for col in data_enw.columns]
-            ].to_numpy().sum()
-        + data_enw.loc[
-            :, ['GuD' in col for col in data_enw.columns]
-            ].to_numpy().sum()
-        - data_enw[(('Elektrizitätsnetzwerk', 'Spotmarkt'), 'flow')].sum()
-        ) * param['param']['elec_consumer_charges_self'])
-
-    cost_el = cost_el_grid + cost_el_internal
-
-    # Erlöse
-    revenues_spotmarkt_timeseries = (np.array(
-        data_enw[(('Elektrizitätsnetzwerk', 'Spotmarkt'), 'flow')])
-                                     * (data['el_spot_price']
-                                        + param['param']['vNNE']))
-    revenues_spotmarkt = revenues_spotmarkt_timeseries.sum()
-
-    revenues_chpbonus = 0
-    if param['BHKW']['active']:
-        revenues_chpbonus += (
-            data_enw[(('BHKW', 'Elektrizitätsnetzwerk'), 'flow')].sum()
-            * (param['BHKW']['chp_bonus'] + param['BHKW']['TEHG_bonus'])
-            )
-    if param['GuD']['active']:
-        revenues_chpbonus += (
-            data_enw[(('GuD', 'Elektrizitätsnetzwerk'), 'flow')].sum()
-            * (param['GuD']['chp_bonus'] + param['GuD']['TEHG_bonus'])
-            )
-    if param['bpt']['active']:
-        revenues_chpbonus += (
-            data_enw[(('BPT', 'Elektrizitätsnetzwerk'), 'flow')].sum()
-            * (param['bpt']['chp_bonus'] + param['bpt']['TEHG_bonus'])
-            )
-
-    revenues_heatdemand = (data_wnw[(('Wärmenetzwerk', 'Wärmebedarf'),
-                                     'flow')].sum()
-                           * param['param']['heat_price'])
-    # Summe der Geldströme
-    Gesamtbetrag = (
-        revenues_spotmarkt + revenues_heatdemand + revenues_chpbonus
-        - cost_Anlagen - cost_gas - cost_el
+    data_dhs, data_invest, data_emission, data_cost_units = postprocessing(
+        results, param, data
         )
 
-        # %% Output Ergebnisse
-
-    # Umbenennen der Spaltennamen der Ergebnisdataframes
-    result_dfs = [
-        data_wnw, data_lt_wnw, data_wnw_node, data_sol_node, data_tes,
-        data_enw, data_gnw
-        ]
-
-    labeldict = generate_labeldict(param)
-    for df in result_dfs:
-        result_labelling(labeldict, df, export_missing_labels=False)
-
-    # Name des Modells
-    # modelname = os.path.basename(__file__)[:-3]
-
-    # Verzeichnes erzeugen, falls nicht vorhanden
-    # if not os.path.exists(os.path.join(dirpath, 'Ergebnisse', modelname)):
-    #     os.mkdir(os.path.join(dirpath, 'Ergebnisse', modelname))
-
-    # Daten zum Plotten der Wärmeversorgung
-    df1 = pd.concat(result_dfs, axis=1)
-    # df1.to_csv(os.path.join(
-    #     dirpath, 'Ergebnisse', modelname, 'data_wnw.csv'),
-    #            sep=";")
-
-    # Daten zum Plotten der Investitionsrechnung
-    df2 = pd.DataFrame(data={'invest_ges': [invest_ges],
-                             'Q_tes': [param['TES']['Q']],
-                             'total_heat_demand': [
-                                 data['heat_demand'].sum()
-                                 * param['param']['rel_demand']
-                                 ],
-                             'Gesamtbetrag': [Gesamtbetrag],
-                             'revenues_spotmarkt': [revenues_spotmarkt],
-                             'revenues_heatdemand': [revenues_heatdemand],
-                             'revenues_chpbonus': [revenues_chpbonus],
-                             'cost_Anlagen': [cost_Anlagen],
-                             'cost_gas': [cost_gas],
-                             'cost_el': [cost_el]})
-    # df2.to_csv(os.path.join(
-    #     dirpath, 'Ergebnisse', modelname, 'data_Invest.csv'),
-    #            sep=";")
-
-    # Daten für die ökologische Bewertung
-    df3 = pd.concat([data_gnw['H_source'],
-                     data_enw[['P_spot_market', 'P_source']]],
-                    axis=1)
-    # df3.to_csv(os.path.join(
-    #     dirpath, 'Ergebnisse', modelname, 'data_CO2.csv'),
-    #            sep=";")
-
-    return df1, df2, df3, cost_df, energysystem.results['meta']
+    return (
+        data_dhs, data_invest, data_emission, data_cost_units,
+        energysystem.results['meta']
+        )
 
 
 if __name__ == '__main__':
---
+++ Generic_Model/Generic_Model_v4/postprocessing.py
@@ -0,0 +1,306 @@
+# -*- coding: utf-8 -*-
+"""
+Economical postprocessing for Generic Model energy system.
+
+@author: Malte Fritz & Jonas Freißmann
+"""
+import pandas as pd
+import numpy as np
+from oemof.solph import views
+from help_funcs import generate_labeldict, result_labelling
+from eco_funcs import invest_sol, invest_stes
+
+
+def postprocessing(results, param, data):
+    """
+    Perform postprocessing of optimization results.
+
+    Parameters
+    ----------
+    results : dict of pandas.Series and pandas.DataFrame
+        Full results from solph.processing.results() call.
+
+    param : dict
+        JSON parameter file of user defined constants.
+
+    data : pandas.DataFrame
+        csv file of user defined time dependent parameters.
+
+    """
+    data_cost_units = pd.DataFrame()
+
+    data_gnw = views.node(results, 'Gasnetzwerk')['sequences']
+    data_enw = views.node(results, 'Elektrizitätsnetzwerk')['sequences']
+    data_wnw = views.node(results, 'Wärmenetzwerk')['sequences']
+    data_lt_wnw = views.node(results, 'LT-Wärmenetzwerk')['sequences']
+    data_wnw_node = views.node(results, 'TES Knoten')['sequences']
+    data_sol_node = views.node(results, 'Sol Knoten')['sequences']
+
+    if param['Sol']['active']:
+        data_solar_source = views.node(results, 'Solarthermie')['sequences']
+        data_cost_units.loc['invest', 'Sol'] = (
+            invest_sol(param['Sol']['A'], col_type='flat')
+            )
+        data_cost_units.loc['op_cost', 'Sol'] = (
+            data_solar_source[(('Solarthermie', 'Sol Knoten'), 'flow')].sum()
+            * 0.01 * data_cost_units.loc['invest', 'Sol']
+            / param['Sol']['A']*data['solar_data'].sum()
+            )
+
+    if param['EHK']['active']:
+        data_cost_units.loc['invest', 'EHK'] = (
+            param['EHK']['inv_spez']
+            * param['EHK']['Q_N']
+            * param['EHK']['amount']
+            )
+
+        for i in range(1, param['EHK']['amount']+1):
+            label_id = 'Elektroheizkessel_' + str(i)
+
+            data_cost_units.loc['op_cost', 'EHK'] = (
+                data_wnw[((label_id, 'Wärmenetzwerk'), 'flow')].sum()
+                * param['EHK']['op_cost_var']
+                + (param['EHK']['op_cost_fix']
+                   * param['EHK']['Q_N'])
+                )
+
+    if param['SLK']['active']:
+        data_cost_units.loc['invest', 'SLK'] = (
+            param['SLK']['inv_spez']
+            * param['SLK']['Q_N']
+            * param['SLK']['amount']
+            )
+
+        for i in range(1, param['SLK']['amount']+1):
+            label_id = 'Spitzenlastkessel_' + str(i)
+
+            data_cost_units.loc['op_cost', 'SLK'] = (
+                data_wnw[((label_id, 'Wärmenetzwerk'), 'flow')].sum()
+                * (param['SLK']['op_cost_var']
+                   + param['param']['energy_tax'])
+                + (param['SLK']['op_cost_fix']
+                   * param['SLK']['Q_N'])
+                )
+
+    if param['BHKW']['active']:
+        if param['BHKW']['type'] == 'constant':
+            ICE_P_max_woDH = param['BHKW']['P_max_woDH']
+        elif param['BHKW']['type'] == 'time series':
+            ICE_P_max_woDH = data['ICE_P_max_woDH'].mean()
+
+        data_cost_units.loc['invest', 'BHKW'] = (
+            ICE_P_max_woDH
+            * param['BHKW']['inv_spez']
+            * param['BHKW']['amount']
+            )
+
+        for i in range(1, param['BHKW']['amount']+1):
+            label_id = 'BHKW_' + str(i)
+
+            data_cost_units.loc['op_cost', 'BHKW'] = (
+                data_enw[((label_id, 'Elektrizitätsnetzwerk'), 'flow')].sum()
+                * param['BHKW']['op_cost_var']
+                + (param['BHKW']['op_cost_fix']
+                   * ICE_P_max_woDH)
+                )
+
+    if param['GuD']['active']:
+        if param['GuD']['type'] == 'constant':
+            CCET_P_max_woDH = param['GuD']['P_max_woDH']
+        elif param['GuD']['type'] == 'time series':
+            CCET_P_max_woDH = data['CCET_P_max_woDH'].mean()
+
+        data_cost_units.loc['invest', 'GuD'] = (
+            CCET_P_max_woDH
+            * param['GuD']['inv_spez']
+            * param['GuD']['amount']
+            )
+
+        for i in range(1, param['GuD']['amount']+1):
+            label_id = 'GuD_' + str(i)
+
+            data_cost_units.loc['op_cost', 'GuD'] = (
+                data_enw[((label_id, 'Elektrizitätsnetzwerk'), 'flow')].sum()
+                * param['GuD']['op_cost_var']
+                + (param['GuD']['op_cost_fix']
+                   * CCET_P_max_woDH)
+                )
+
+    if param['HP']['active']:
+        if param['HP']['type'] == 'constant':
+            HP_Q_N = (param['HP']['c_1']
+                      * param['HP']['P_max']
+                      + param['HP']['c_0'])
+        elif param['HP']['type'] == 'time series':
+            HP_Q_N = (data['c_1_hp'].mean()
+                      * data['P_max_hp'].mean()
+                      + data['c_0_hp'].mean())
+        data_cost_units.loc['invest', 'HP'] = (
+            param['HP']['inv_spez']
+            * data['P_max_hp'].max()
+            * HP_Q_N
+            * param['HP']['amount']
+            )
+        for i in range(1, param['HP']['amount']+1):
+            label_id = 'HP_' + str(i)
+
+            data_cost_units.loc['op_cost', 'HP'] = (
+                data_wnw[((label_id, 'Wärmenetzwerk'), 'flow')].sum()
+                * param['HP']['op_cost_var']
+                + param['HP']['op_cost_fix'] * HP_Q_N
+                )
+
+    if param['LT-HP']['active']:
+        LT_HP_Q_N = data_wnw[((label_id, 'Wärmenetzwerk'), 'flow')].mean()
+
+        for i in range(1, param['LT-HP']['amount']+1):
+            label_id = 'LT-HP_' + str(i)
+
+            data_cost_units.loc['invest', 'LT-HP'] = (
+                param['HP']['inv_spez'] * LT_HP_Q_N
+                )
+
+            data_cost_units.loc['op_cost', 'LT-HP'] = (
+                data_wnw[((label_id, 'Wärmenetzwerk'), 'flow')].sum()
+                * param['HP']['op_cost_var']
+                + param['HP']['op_cost_fix'] * LT_HP_Q_N
+                )
+
+    data_tes = pd.DataFrame()
+    if param['TES']['active']:
+        data_cost_units.loc['invest', 'TES'] = (
+            invest_stes(param['TES']['Q'])
+            * param['TES']['amount']
+            )
+
+        for i in range(1, param['TES']['amount']+1):
+            label_id = 'TES_' + str(i)
+            data_tes = pd.concat([data_tes,
+                                  views.node(results, label_id)['sequences']],
+                                 axis=1)
+
+            data_cost_units.loc['op_cost', 'TES'] = (
+                data_tes[(('TES Knoten', label_id), 'flow')].sum()
+                * param['TES']['op_cost_var']
+                + (param['TES']['op_cost_fix']
+                   * param['TES']['Q'])
+                )
+
+    if param['ST-TES']['active']:
+        data_cost_units.loc['invest', 'ST-TES'] = (
+            invest_stes(param['ST-TES']['Q'])
+            * param['ST-TES']['amount']
+            )
+
+        for i in range(1, param['ST-TES']['amount']+1):
+            label_id = 'ST-TES_' + str(i)
+            data_tes = pd.concat([data_tes,
+                                  views.node(results, label_id)['sequences']],
+                                 axis=1)
+
+            data_cost_units.loc['op_cost', 'ST-TES'] = (
+                data_tes[(('Wärmenetzwerk', label_id), 'flow')].sum()
+                * param['ST-TES']['op_cost_var']
+                + (param['ST-TES']['op_cost_fix']
+                   * param['ST-TES']['Q'])
+                )
+
+    cost_Anlagen = data_cost_units.loc['op_cost'].sum()
+    invest_ges = data_cost_units.loc['invest'].sum()
+
+    cost_gas = (data_gnw[(('Gasquelle', 'Gasnetzwerk'), 'flow')].sum()
+                * (param['param']['gas_price']
+                   + (param['param']['co2_price']
+                      * param['param']['ef_gas'])))
+
+    specific_costs_el_grid = (
+        param['param']['elec_consumer_charges_grid']
+        + data['el_spot_price']
+        + (param['param']['co2_price']
+           * data['ef_om'])
+        )
+    cost_el_grid = (
+        np.array(data_enw[(('Stromquelle', 'Elektrizitätsnetzwerk'), 'flow')])
+        * specific_costs_el_grid)
+    cost_el_grid = cost_el_grid.sum()
+
+    cost_el_internal = ((
+        data_enw.loc[
+            :, ['BHKW' in col for col in data_enw.columns]
+            ].to_numpy().sum()
+        + data_enw.loc[
+            :, ['GuD' in col for col in data_enw.columns]
+            ].to_numpy().sum()
+        - data_enw[(('Elektrizitätsnetzwerk', 'Spotmarkt'), 'flow')].sum()
+        ) * param['param']['elec_consumer_charges_self'])
+
+    cost_el = cost_el_grid + cost_el_internal
+
+    revenues_spotmarkt_timeseries = (np.array(
+        data_enw[(('Elektrizitätsnetzwerk', 'Spotmarkt'), 'flow')])
+                                     * (data['el_spot_price']
+                                        + param['param']['vNNE']))
+    revenues_spotmarkt = revenues_spotmarkt_timeseries.sum()
+
+    revenues_chpbonus = 0
+    if param['BHKW']['active']:
+        revenues_chpbonus += (
+            data_enw[(('BHKW', 'Elektrizitätsnetzwerk'), 'flow')].sum()
+            * (param['BHKW']['chp_bonus'] + param['BHKW']['TEHG_bonus'])
+            )
+    if param['GuD']['active']:
+        revenues_chpbonus += (
+            data_enw[(('GuD', 'Elektrizitätsnetzwerk'), 'flow')].sum()
+            * (param['GuD']['chp_bonus'] + param['GuD']['TEHG_bonus'])
+            )
+    if param['bpt']['active']:
+        revenues_chpbonus += (
+            data_enw[(('BPT', 'Elektrizitätsnetzwerk'), 'flow')].sum()
+            * (param['bpt']['chp_bonus'] + param['bpt']['TEHG_bonus'])
+            )
+
+    revenues_heatdemand = (data_wnw[(('Wärmenetzwerk', 'Wärmebedarf'),
+                                     'flow')].sum()
+                           * param['param']['heat_price'])
+
+    revenues_total = (
+        revenues_spotmarkt + revenues_heatdemand + revenues_chpbonus
+        )
+    cost_total = cost_Anlagen + cost_gas + cost_el
+    Gesamtbetrag = revenues_total - cost_total
+
+    result_dfs = [
+        data_wnw, data_lt_wnw, data_wnw_node, data_sol_node, data_tes,
+        data_enw, data_gnw
+        ]
+
+    labeldict = generate_labeldict(param)
+    for df in result_dfs:
+        result_labelling(labeldict, df, export_missing_labels=False)
+
+    data_dhs = pd.concat(result_dfs, axis=1)
+
+    data_invest = pd.DataFrame(data={
+        'invest_ges': [invest_ges],
+        'Q_tes': [param['TES']['Q']],
+        'total_heat_demand': [
+            data['heat_demand'].sum() * param['param']['rel_demand']
+            ],
+        'Gesamtbetrag': [Gesamtbetrag],
+        'revenues_spotmarkt': [revenues_spotmarkt],
+        'revenues_heatdemand': [revenues_heatdemand],
+        'revenues_chpbonus': [revenues_chpbonus],
+        'cost_Anlagen': [cost_Anlagen],
+        'cost_gas': [cost_gas],
+        'cost_el': [cost_el],
+        'cost_el_grid': [cost_el_grid],
+        'cost_el_internal': [cost_el_internal],
+        'cost_total': [cost_total],
+        'revenues_total': [revenues_total]
+        })
+
+    data_emission = pd.concat([
+        data_gnw['H_source'], data_enw[['P_spot_market', 'P_source']]
+        ], axis=1)
+
+    return data_dhs, data_invest, data_emission, data_cost_units
